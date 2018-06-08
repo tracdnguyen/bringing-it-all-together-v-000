5bdaa3fe957cee8aef93faab5ab1ae4323a672cb
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 22:35:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,7 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE dogs (
+      CREATE TABLE IF NOT EXISTS dogs (
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
