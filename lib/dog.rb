3dd04654ef1e756efe42c6e2227abd096621080f
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 22:36:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,4 +22,15 @@ class Dog
     sql = "DROP TABLE dogs"
     DB[:conn].execute(sql)
   end
+
+  def save
+    sql = <<-SQL
+       INSERT INTO students (name, grade) 
+       VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
 end
