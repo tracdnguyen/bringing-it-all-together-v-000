3f2b1ac57dd1b77c203b49004fb3e2f0320437fb
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 22:32:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,14 @@ class Dog
     @name = name
     @breed = breed
   end
+
+  def ::create_table 
+    sql = <<-SQL
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      );
+    SQL
+  end
 end
