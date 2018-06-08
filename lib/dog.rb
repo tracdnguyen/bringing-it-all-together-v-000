52aaa01e75a8589590661db73dc3afb9ea8c2265
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 23:14:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,4 +65,12 @@ class Dog
   def self::new_from_db(row)
     self.find_or_create_by(name: row[1], breed: row[2])
   end
+
+  def self::find_by_name(name:)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+
+    result = DB[:conn].execute(sql, name)[0][0]
+
+    self.find_by_id(result)
+  end
 end
