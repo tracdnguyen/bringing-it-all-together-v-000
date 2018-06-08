7a66c86453dd9ab8135f8430f553ce407cf577c3
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 22:40:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,13 +24,16 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-       INSERT INTO dogs (name, breed)
-       VALUES (?, ?)
-    SQL
-
-    result = DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    result
+    if self.id 
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL
+ 
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
   end
 end
