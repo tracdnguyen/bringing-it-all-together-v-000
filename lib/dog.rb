bef98dd55d8d6f9bfa9ab90e4dbb8790fddee7a2
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 22:49:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,6 +44,12 @@ class Dog
     dog
   end
 
+  def self::find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    self.new(result[0], result[1], result[2])
+  end
+
   def self::new_from_db
 
   end
